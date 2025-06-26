e3ec3741b2a9f2d6284f6f5f7dddcea82b93fd05
Brijeshkumar Yadav <brijeshkumaryadav2026@gmail.com>
2025-06-27 01:57:03 +0530

    chore: update dependencies and add Vercel analytics and speed insights

--- apps/docs/src/app/layout.tsx
+++ apps/docs/src/app/layout.tsx
@@ -1,4 +1,6 @@
 import { generateSEOMetadata } from '@/lib/seo';
+import { Analytics } from '@vercel/analytics/react';
+import { SpeedInsights } from "@vercel/speed-insights/next";
 import { RootProvider } from 'fumadocs-ui/provider';
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
@@ -16,6 +18,8 @@ export default function Layout({ children }: { children: ReactNode }) {
     <html lang="en" className={inter.className} suppressHydrationWarning>
       <body className="flex flex-col min-h-screen">
         <RootProvider>{children}</RootProvider>
+        <Analytics />
+        <SpeedInsights/>
       </body>
     </html>
   );
