34ff7fefa307f32468a23260bfb6560b13cdfc74
Brijeshkumar Yadav <brijeshkumaryadav2026@gmail.com>
2025-07-06 12:24:13 +0530

    fix: simplify baseUrl logic to use a static production URL

--- apps/docs/src/lib/metadata.ts
+++ apps/docs/src/lib/metadata.ts
@@ -29,6 +29,6 @@ export function createMetadata(override: Metadata): Metadata {
 }
 
 export const baseUrl =
-  process.env.NODE_ENV === "development" || !process.env.VERCEL_URL
+  process.env.NODE_ENV === "development"
     ? new URL("http://localhost:3000")
-    : new URL(`https://${process.env.VERCEL_URL}`);
+    : new URL("https://react-usekit.vercel.app");
